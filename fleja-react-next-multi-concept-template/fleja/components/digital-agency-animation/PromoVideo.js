552e7bebd822de9388874eab8a8fb0d39e7132aa
Rikin <dolllyamar@gmail.com>
2020-08-29 14:53:02 -0400

    Fixed PromoVideo

--- fleja-react-next-multi-concept-template/fleja/components/digital-agency-animation/PromoVideo.js
+++ fleja-react-next-multi-concept-template/fleja/components/digital-agency-animation/PromoVideo.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Link from 'next/link';
+import Embed from "react-embed";
 
 export class PromoVideo extends Component {
     render() {
@@ -8,62 +8,33 @@ export class PromoVideo extends Component {
                 <div className="d-table">
                     <div className="d-table-cell">
                         <div className="container">
-                            <div className="row h-100 align-items-center">
-                                <div className="col-lg-6 col-md-12">
-                                    <div className="hero-content">
-                                        <div dangerouslySetInnerHTML={`<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/fiJIP2-Tylw\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>`}/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 col-md-12">
-                                    <div className="hero-content">
-                                        <h2 className="wow fadeInUp">What is UTCG?</h2>
-                                    </div>
+                            <div className="row h-100 justify-content-center align-items-center">
+                                <div className="col-lg-8 col-md-12 hero-content">
+                                    <Embed url="https://youtu.be/fiJIP2-Tylw"/>
+                                    <h2 className="wow fadeInUp">What is UTCG?</h2>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="shape1">
-                    <img src={require("../../images/shapes/cube_corner.png")} alt="shape"/>
+                <div className="shape13">
+                    <img src={require("../../images/shapes/13.png")} alt="shape" />
                 </div>
-                <div className="shape2">
-                    <img src={require("../../images/shapes/corner_top_rt.png")} alt="shape"/>
+                <div className="shape18">
+                    <img src={require("../../images/shapes/18.png")} alt="shape" />
                 </div>
-                <div className="shape3">
-                    <img src={require("../../images/shapes/corner_bot_left.png")} alt="shape"/>
+                <div className="shape19">
+                    <img src={require("../../images/shapes/19.png")} alt="shape" />
                 </div>
-                <div className="shape4">
-                    <img src={require("../../images/shapes/4.png")} alt="shape"/>
+                <div className="shape20 rotateme">
+                    <img src={require("../../images/shapes/20.png")} alt="shape" />
                 </div>
-                <div className="shape5">
-                    <img src={require("../../images/shapes/5.png")} alt="shape"/>
+                <div className="shape21">
+                    <img src={require("../../images/shapes/21.png")} alt="shape" />
                 </div>
-                <div className="shape6">
-                    <img src={require("../../images/shapes/6.png")} alt="shape"/>
-                </div>
-                <div className="shape7">
-                    <img src={require("../../images/shapes/7.png")} alt="shape"/>
-                </div>
-                <div className="shape8">
-                    <img src={require("../../images/shapes/8.png")} alt="shape"/>
-                </div>
-                <div className="shape9 rotateme">
-                    <img src={require("../../images/shapes/9.png")} alt="shape"/>
-                </div>
-                <div className="shape10 rotateme">
-                    <img src={require("../../images/shapes/10.png")} alt="shape"/>
-                </div>
-
-
-                <div className="circle-shape1">
-                    <img src={require("../../images/shapes/circle1.png")} alt="shape"/>
-                </div>
-                <div className="circle-shape2">
-                    <img src={require("../../images/shapes/circle2.png")} alt="shape"/>
-                </div>
-                <div className="circle-shape3">
-                    <img src={require("../../images/shapes/circle3.png")} alt="shape"/>
+                <div className="shape22">
+                    <img src={require("../../images/shapes/22.png")} alt="shape" />
                 </div>
             </div>
         );
