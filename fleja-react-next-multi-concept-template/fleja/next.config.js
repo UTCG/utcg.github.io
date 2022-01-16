d690269f687e9c6ef20d835110eb195fa2c8feca
Rikin <dolllyamar@gmail.com>
2022-01-16 01:29:27 -0500

    Update to hopefully finally build

--- fleja-react-next-multi-concept-template/fleja/next.config.js
+++ fleja-react-next-multi-concept-template/fleja/next.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-    // sassOptions: {
-    //     includePaths: [path.join(__dirname, 'styles')],
-    // },
-    // trailingSlash: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    trailingSlash: true,
 }
