1f27a3fe383e72a573d5ce48b20d21500b17499e
rikin <dolllyamar@gmail.com>
2022-09-11 01:58:38 -0400

    Added discord redirect url, updated packages hopefully in an ok way

---
+++ fleja-react-next-multi-concept-template/fleja/pages/discord.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+
+function discord() {
+    useEffect(() => { window.location.href = "https://discord.gg/444b3rh2dn" }, []);
+    return <></>;
+}
+
+export default discord;
