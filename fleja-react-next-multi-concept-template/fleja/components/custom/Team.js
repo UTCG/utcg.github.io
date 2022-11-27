2cbca351596d3e9fd935cca01a42fde68702031f
sheldondaquan <54724209+sheldondaquan@users.noreply.github.com>
2022-11-27 11:46:30 -0500

    Add files via upload

---
+++ fleja-react-next-multi-concept-template/fleja/components/custom/Team.js
@@ -0,0 +1,212 @@
+import React, { Component } from "react";
+
+export class Team extends Component {
+  render() {
+    return (
+      <section className="team-area bg-image ptb-120">
+        <div className="container">
+          <div className="section-title">
+            <span className="wow fadeInUp">Team Members</span>
+            <h2 className="wow fadeInDown">Meet UTCG's Executive Team</h2>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src="/images/team-image/2021-team-images/rikin.png"
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src="/images/team-image/team-back.jpg"
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links">
+                    <li>
+                      <a href="https://rikingurditta.github.io">
+                        <i className="icofont-home"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/rikin-gurditta">
+                        <i className="icofont-linkedin"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/rikingurditta">
+                        <i className="icofont-github"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Rikin Gurditta</h3>
+                  <span>Co-President</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src={"/images/louis.jpg"}
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src={"/images/team-image/team-back.jpg"}
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links">
+                    <li>
+                      <a href="https://github.com/AX-I">
+                        <i className="icofont-github"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Louis Zhang</h3>
+                  <span>Co-President</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src={"/images/team-image/2021-team-images/doris.jpg"}
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src={"/images/team-image/team-back.jpg"}
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links"></ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Doris Ni</h3>
+                  <span>Marketing Director</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src={"/images/team-image/2021-team-images/kurtis.jpg"}
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src={"/images/team-image/team-back.jpg"}
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links">
+                    <li>
+                      <a href="https://www.linkedin.com/in/kurtislaw">
+                        <i className="icofont-linkedin"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/kurtislaw">
+                        <i className="icofont-github"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Kurtis Law</h3>
+                  <span>General Executive Member</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src="/images/team-image/2021-team-images/edith.jpg"
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src="/images/team-image/team-back.jpg"
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links"></ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Edith Ma</h3>
+                  <span>General Executive Member</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+              <div className="single-team">
+                <div className="team-image">
+                  <img
+                    src="/images/team-image/2021-team-images/luwai.png"
+                    className="member-img"
+                    alt="team"
+                  />
+                  <img
+                    src="/images/team-image/team-back.jpg"
+                    className="back-image"
+                    alt="back-image"
+                  />
+
+                  <ul className="social-links"></ul>
+                </div>
+
+                <div className="team-content">
+                  <h3>Lu-Wai Wong</h3>
+                  <span>Marketing Executive Member</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="circle-top rotateme">
+          <img src="/images/circle-shape.png" alt="circle" />
+        </div>
+        <div className="shape2">
+          <img src="/images/shapes/2.png" alt="shape" />
+        </div>
+        <div className="shape10 rotateme">
+          <img src="/images/shapes/10.png" alt="shape" />
+        </div>
+        <div className="circle-bottom rotateme">
+          <img src="/images/circle-shape.png" alt="circle" />
+        </div>
+        <div className="shape6">
+          <img src="/images/shapes/6.png" alt="shape" />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Team;
