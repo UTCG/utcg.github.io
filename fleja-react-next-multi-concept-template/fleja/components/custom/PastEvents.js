c991bc29c074e410c3d8741a27ab4646790414af
Kurtis Law <kurtislaww@gmail.com>
2022-12-29 14:24:34 -0800

    added new events from 2022 (links not updated yet)

--- fleja-react-next-multi-concept-template/fleja/components/custom/PastEvents.js
+++ fleja-react-next-multi-concept-template/fleja/components/custom/PastEvents.js
@@ -26,7 +26,112 @@ export class BlogBody extends Component {
                         updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
                     >
 
+                    
 
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/IMG_6193.jpg"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">Workshop</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">28 Nov, 2022</span>
+								<h3><a href="https://www.eventbrite.ca/e/utcg-shaders-workshop-tickets-474947228947">Minecraft Shaders Workshop</a></h3>
+								<a href="https://www.eventbrite.ca/e/utcg-shaders-workshop-tickets-474947228947" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/bunny-social.png"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">Workshop</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">23 Nov, 2022</span>
+								<h3><a href="https://www.eventbrite.ca/e/utcg-shaders-workshop-tickets-474947228947">Bunny Social</a></h3>
+								<a href="https://www.eventbrite.ca/e/utcg-shaders-workshop-tickets-474947228947" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+
+                    
+
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/IMG_6120.jpg"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">VFX</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">16 Nov, 2022</span>
+								<h3><a href="https://www.facebook.com/events/875874843302115/">Halo: A VFX Breakdown</a></h3>
+								<a href="https://www.facebook.com/events/875874843302115/" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/IMG_5979.jpg"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">Social</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">24 Oct, 2022</span>
+								<h3><a href="https://www.facebook.com/events/875874843302115/">Siggraph Viewing Social</a></h3>
+								<a href="https://www.facebook.com/events/875874843302115/" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+
+                    
+
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/IMG_5936.jpg"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">Social</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">13 Oct, 2022</span>
+								<h3><a href="https://www.facebook.com/events/875874843302115/">Model UofT</a></h3>
+								<a href="https://www.facebook.com/events/875874843302115/" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+                    
                     <div className="col-lg-4 col-md-6 grid-items">
 						<div className="single-blog-post">
 							<div className="blog-image">
@@ -40,8 +145,28 @@ export class BlogBody extends Component {
 							</div>
 
 							<div className="blog-post-content">
-								<span className="date">29 Oct., 2021</span>
+								<span className="date">29 Oct, 2021</span>
 								<h3><a href="https://www.facebook.com/events/875874843302115/">Blender Violin Workshop</a></h3>
+								<a href="https://www.facebook.com/events/875874843302115/" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
+							</div>
+						</div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 grid-items">
+						<div className="single-blog-post">
+							<div className="blog-image">
+								<a href="#">
+									<img src={"/images/b-workshop.png"} alt="image" />
+								</a>
+
+								<div className="post-tag">
+									<a href="#">Workshop</a>
+								</div>
+							</div>
+
+							<div className="blog-post-content">
+								<span className="date">28 Sep, 2021</span>
+								<h3><a href="https://www.facebook.com/events/875874843302115/">Blender Workshop: Fundamentals of 3D Graphics</a></h3>
 								<a href="https://www.facebook.com/events/875874843302115/" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
 							</div>
 						</div>
@@ -60,7 +185,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">27 Feb., 2020</span>
+                                <span className="date">27 Feb, 2020</span>
                                 <h3><a href="#">Rapid Fire Research Talks</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -80,7 +205,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">31 Jan., 2020</span>
+                                <span className="date">31 Jan, 2020</span>
                                 <h3><a href="#">The Mandalorian: A VFX Breakdown</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -100,7 +225,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">9 Jan., 2020</span>
+                                <span className="date">9 Jan, 2020</span>
                                 <h3><a href="#">SIGGRAPH 2020 Student Volunteer Info Session</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -120,7 +245,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">12 Nov., 2019</span>
+                                <span className="date">12 Nov, 2019</span>
                                 <h3><a href="#">Computer Graphics at Ubisoft</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -140,7 +265,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">30 Oct., 2019</span>
+                                <span className="date">30 Oct, 2019</span>
                                 <h3><a href="#">VFX Behind Star Trek</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -160,7 +285,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">15 Oct., 2019</span>
+                                <span className="date">15 Oct, 2019</span>
                                 <h3><a href="#">Computer Graphics Job Fair</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -180,7 +305,7 @@ export class BlogBody extends Component {
                             </div>
 
                             <div className="blog-post-content">
-                                <span className="date">13 May., 2019</span>
+                                <span className="date">13 May, 2019</span>
                                 <h3><a href="#">Computer Animation Festival Screening</a></h3>
                                 <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                             </div>
@@ -220,7 +345,7 @@ export class BlogBody extends Component {
                                 </div>
 
                                 <div className="blog-post-content">
-                                    <span className="date">14 Mar., 2019</span>
+                                    <span className="date">14 Mar, 2019</span>
                                     <h3><a href="#">Autodesk Office Tour</a></h3>
                                     <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                                 </div>
@@ -240,7 +365,7 @@ export class BlogBody extends Component {
                                 </div>
 
                                 <div className="blog-post-content">
-                                    <span className="date">6 Mar., 2019</span>
+                                    <span className="date">6 Mar, 2019</span>
                                     <h3><a href="#">Intro to Azure Custom Vision</a></h3>
                                     <a href="#" className="read-more-btn">Read More <i className="icofont-double-right"></i></a>
                                 </div>
